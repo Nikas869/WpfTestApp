cd2cb02555723044aab01f70a1340b9df46ec4bc
Nikita Magda <nikmagdaa@gmail.com>
2017-09-24 17:06:52 +0300

    Chapter 2

--- WpfTestApp/MainWindow.xaml.cs
+++ WpfTestApp/MainWindow.xaml.cs
@@ -15,14 +15,30 @@ using System.Windows.Shapes;
 
 namespace WpfTestApp
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show((button1.Content as Phone)?.ToString());
+        }
     }
-  }
+
+    public class Phone
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Смартфон {Name}; цена: {Price}";
+        }
+    }
 }
